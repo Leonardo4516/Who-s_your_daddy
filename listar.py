6fb0d5f614c49b2f9b0c532f8c23409fe090e77f
Cristixn04 <247783786+Cristixn04@users.noreply.github.com>
2026-02-17 08:36:39 -0500

    feat: :sparkles: Funcion para listar ramas

---
+++ listar.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+
+def listado():
+    print("\n[FORMATO JSON]")
+    try:
+        with open("parientes.json","r", encoding="utf-8") as archivo_json:
+            datos = json.load(archivo_json)
+            for contact in datos:
+                print(f"""
+                      =====================================
+                      Nombre: {contact['nombre']}
+                      Parentesco: {contact['parentesco']}
+                      Edad: {contact['edad']}
+                      =====================================
+                      """)
+    except FileNotFoundError:
+        print("El archivo JSON aun no existe.")
+    except Exception as e:
+        print(f"Error inesperado en lectura de JSON: {e}")
+    except json.JSONDecodeError as e:
+        print(f"Error al leer el archivo JSON: {e}")
