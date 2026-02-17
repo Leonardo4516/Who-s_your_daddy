29da93e28194479e6b21e8a6bd62a6f4e5fabc27
Leonardo4516 <lehernan.07@gmail.com>
2026-02-16 21:25:36 -0500

    feat: :sparkles: Cierre de sesión creado

---
+++ cierre_sesion.py
@@ -0,0 +1,6 @@
+def cerrar_sesion():
+    print("======================================")
+    print("Cerrando sesión...")
+    print("======================================")
+    print("Sesión cerrada exitosamente.")
+    print("======================================")
